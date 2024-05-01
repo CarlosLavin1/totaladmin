d99930502ccce4971f39f7ac1b9fc306ab8de73a
Isaactaken <IKISEMBO01@mynbcc.ca>
2024-05-01 11:03:42 -0300

    added PO model

---
+++ TotalAdmin/TotalAdmin.Model/Entities/PurchaseOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalAdmin.Model.Entities
+{
+    public class PurchaseOrder : BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PoNumber { get; set; }
+
+        [Display(Name = "Creation Date")]
+        [Required(ErrorMessage = "Please enter a Creation date.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreationDate { get; set; }
+
+        [ConcurrencyCheck]
+        public int RowVersion { get; set; }
+
+        [Display(Name = "Employee Number")]
+        [Required(ErrorMessage = "Please select a employee.")]
+        public int EmployeeNumber { get; set; }
+
+        [Display(Name = "Status")]
+        [Required(ErrorMessage = "Please select a status.")]
+        public int StatusId { get; set; }
+
+    }
+}
