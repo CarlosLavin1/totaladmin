66e60e9ac869cd1797ea562c9676177393321afd
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2024-05-01 20:55:12 -0300

    added employee repo

---
+++ TotalAdmin/TotalAdmin.Repository/EmployeeRepository.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TotalAdmin.Model;
+
+namespace TotalAdmin.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly IDataAccess db;
+
+        public EmployeeRepository(IDataAccess db)
+        {
+            this.db = db;
+        }
+
+        public Employee AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetEmployeeList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeDisplayDTO> SearchEmployees(string? department, int employeeNumber, string? name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TotalAdmin/TotalAdmin.Repository/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TotalAdmin.Model;
+
+namespace TotalAdmin.Repository
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployeeById(int id);
+        List<Employee> GetEmployeeList();
+        Employee AddEmployee(Employee employee);
+        List<EmployeeDisplayDTO> SearchEmployees(string? department, int employeeNumber, string? name);
+    }
+}
