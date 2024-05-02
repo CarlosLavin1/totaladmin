3f2f3651c26fd148fed81abe09df9ccc693a9fa4
Isaactaken <IKISEMBO01@mynbcc.ca>
2024-05-02 01:50:57 -0300

    Added DTOs

---
+++ TotalAdmin/TotalAdmin.Model/DTO/PODisplayDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalAdmin.Model.DTO
+{
+    public class PODisplayDTO
+    {
+        public int PoNumber { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Status { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Tax { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
