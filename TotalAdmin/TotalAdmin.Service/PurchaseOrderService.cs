bee2386f57c2f2a2deffbe57279b1d4c1c1ff716
Isaactaken <IKISEMBO01@mynbcc.ca>
2024-05-02 01:57:22 -0300

    added Purchase order Model, Service and API controller

---
+++ TotalAdmin/TotalAdmin.Service/PurchaseOrderService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TotalAdmin.Model.DTO;
+using TotalAdmin.Model.Entities;
+using TotalAdmin.Repository;
+using TotalAdmin.Types;
+
+namespace TotalAdmin.Service
+{
+    public class PurchaseOrderService
+    {
+        public readonly PurchaseOrderRepo repo = new();
+
+        public async Task<PurchaseOrder> AddPurchaseOrder(PurchaseOrder purchaseOrder)
+        {
+            if (ValidatePurchaseOrder(purchaseOrder))
+                return await repo.AddAsync(purchaseOrder);
+
+            return purchaseOrder;
+        }
+       
+        public async Task<List<PurchaseOrder>> GetPurchaseOrdersForEmployee(int employeeNumber)
+        {
+            return await repo.ReviewEmployeePO(employeeNumber);
+        }
+
+        public async Task<List<POSearchResultsApiDTO>> GetPuchaseOrdersForDepartment(int departmentId)
+        {
+            return await repo.GetPOSearchResults(departmentId);
+        }
+
+
+        public async Task<List<PODisplayDTO>> SearchPurchaseOrders(POSearchFiltersDTO filter)
+        {
+            return await repo.SearchPurchaseOrders(filter);
+        }
+
+
+        private bool ValidatePurchaseOrder(PurchaseOrder po)
+        {
+            // validate entity
+            List<ValidationResult> results = new();
+            bool isValid = Validator.TryValidateObject(po, new ValidationContext(po), results, true);
+
+            foreach (ValidationResult e in results)
+            {
+                po.AddError(new ValidationError(e?.ErrorMessage ?? "null", ErrorType.Model));
+            }
+
+            return isValid;
+        }
+    }
+}
