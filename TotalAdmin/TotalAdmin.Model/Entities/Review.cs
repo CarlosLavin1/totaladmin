f8658085b896417646a336c0f65ec019b195a6de
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2024-05-13 12:50:54 -0300

    added review model

---
+++ TotalAdmin/TotalAdmin.Model/Entities/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalAdmin.Model 
+{ 
+    public class Review
+    {
+        public int Id { get; set; }
+        public string? Rating { get; set; }
+        public string? Comment { get; set; }
+        public int Quarter { get; set; }
+        public DateTime? Date { get; set; }
+        public int EmployeeNumber { get; set; }
+        public int SupervisorEmployeeNumber { get; set; }
+    }
+}
